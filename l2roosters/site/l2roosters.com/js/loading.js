574cc549bc811a166aff661a2438d8f477ee1c69
max53nerfed <max53nerfed@inbox.lv>
2025-07-05 21:55:05 -0300

    testing sites

---
+++ l2roosters/site/l2roosters.com/js/loading.js
@@ -0,0 +1,9 @@
+$(window).on('load', function () {
+    $('#loading').fadeOut(500);
+    $('.loading').addClass('scale-loading');
+    $('.loading-logo').addClass('scale-loading');
+    $('.loading').css('opacity', '0');
+    $('.loading-logo').css('opacity', '0');
+    $('.loading-progress-bar').css('opacity', '0');
+    $('.loading-text').css('opacity', '0');
+  }) 
